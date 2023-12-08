877aaed7378ce09f97b8cb28f24ee9445879b303
Royaloak <Royaloak@EMS-UmaMaheshwari>
2023-12-08 15:21:52 +0530

    commit2.

--- royalOak_CX_Flow/src/royalOak_CX_Flow/RoyalOakProductSearch_Keyword.java
+++ royalOak_CX_Flow/src/royalOak_CX_Flow/RoyalOakProductSearch_Keyword.java
@@ -21,7 +21,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class RoyalOakProductSearch_Keyword {
 
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws InterruptedException{
 		WebDriver driver = new ChromeDriver();
 		driver.navigate().to("https://www.royaloakindia.com/");
 		driver.manage().window().maximize();
